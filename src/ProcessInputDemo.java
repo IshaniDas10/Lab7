58c29a93c5758228790c2f7136e89e213090b6d9
Ishani das <24157020@kiit.ac.in>
2026-01-29 16:43:27 +0530

    commit

---
+++ src/ProcessInputDemo.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class NegativeNumberException extends Exception {
+    public NegativeNumberException(String message) {
+        super(message);
+    }
+}
+
+public class ProcessInputDemo {
+
+    void ProcessInput(int num) throws NegativeNumberException {
+        if (num < 0) {
+            throw new NegativeNumberException("number should be positive");
+        } else {
+            System.out.println("Double value: " + (num * 2));
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ProcessInputDemo obj = new ProcessInputDemo();
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        try {
+            obj.ProcessInput(n);
+        } catch (NegativeNumberException e) {
+            System.out.println("Caught the exception");
+            System.out.println("Exception occurred: " + e);
+        }
+    }
+}
